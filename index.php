8c680573042d43a92343524484976feb3be94820
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-05-15 20:25:55 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php require_once './php_components/header.php'; ?>
+<div class="container">
+  <h1>PlantCare - Take care of Your plants!</h1>
+  <a class="button primary-btn" href="login.php">Log In</a>
+  <a class="button primary-btn" href="register.php">Register</a>
+</div>
+<?php require_once './php_components/footer.php'; ?>
