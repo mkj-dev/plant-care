fcd2b85311e3db8b11b5156bb670cd1ea49b4538
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-05-15 20:25:01 +0200

    Create header component

---
+++ php_components/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PlantCare - Take care of Your plants!</title>
+  <meta name="description" content="PlantCare is an app that helps you take care of your houseplants. Add your flowers, track their needs for soil, sunlight, watering and repotting. Log in or register to start using the PlantCare.">
+  <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
