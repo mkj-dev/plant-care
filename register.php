63aedac28d22a72719e8cc5ae1fc48d1f2f77350
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-05-16 15:39:11 +0200

    Code refactoring - using the UserRepository class

--- register.php
+++ register.php
@@ -1,56 +1,41 @@
 <?php
+require_once './database/connection.php';
+require_once './database/UserRepository.php';
 require_once './php_components/header.php';
-require_once 'connection.php';
 
 $errors = [];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   if (!empty($_POST['username']) && !empty($_POST['password']) && !empty($_POST['email'])) {
     $username = htmlspecialchars(trim($_POST['username']));
-    $password = password_hash(htmlspecialchars(trim($_POST['password'])), PASSWORD_DEFAULT);
+    $password = htmlspecialchars(trim($_POST['password']));
     $email = htmlspecialchars(trim($_POST['email']));
 
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
       $errors[] = "Invalid email format.";
     }
 
-    if (empty($errors)) {
-      try {
-        // Check if the username and email are unique
-        $checkUsername = $db_conn->prepare('SELECT COUNT(*) FROM users WHERE username = :username');
-        $checkUsername->execute(['username' => $username]);
-        $usernameCount = $checkUsername->fetchColumn();
+    try {
+      $userRepository = new UserRepository($db_conn);
 
-        if ($usernameCount > 0) {
-          $errors[] = "Username already exists.";
-        }
+      // Check if username or email already exists
+      $existingUser = $userRepository->getUserByUsername($username);
+      $existingEmail = $userRepository->getUserByEmail($email);
 
-        $checkEmail = $db_conn->prepare('SELECT COUNT(*) FROM users WHERE email = :email');
-        $checkEmail->execute(['email' => $email]);
-        $emailCount = $checkEmail->fetchColumn();
-
-        if ($emailCount > 0) {
-          $errors[] = "Email already exists.";
-        }
-
-        if (empty($errors)) {
-          $newUser = $db_conn->prepare('INSERT INTO users (username, password, email) VALUES (:username, :password, :email)');
-          $newUser->execute(
-            [
-              'username' => $username,
-              'password' => $password,
-              'email' => $email
-            ]
-          );
-          echo "<h3 class='success'>Your account has been created!</h3>";
-        }
-      } catch (Exception $e) {
-        $errors[] = "An error occurred while creating the account.";
+      if ($existingUser) {
+        $errors[] = "Username already exists.";
       }
+
+      if ($existingEmail) {
+        $errors[] = "Email already exists.";
+      }
+    } catch (Exception $e) {
+      // TODO log the exception to a file
+      $errors[] = "An error occurred while creating the account.";
     }
-  } else {
-    $errors[] = "All fields are required!";
   }
+} else {
+  $errors[] = "All fields are required!";
 }
 ?>
 
@@ -74,13 +59,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <form action="/index.php">
     <button type="submit" class="button secondary-btn">Go back</button>
   </form>
-</div>
 <?php
 if (!empty($errors)) {
   foreach ($errors as $error) {
-    echo "<p class='error'>Error: $error</p>";
+    echo "<h3 class='error'>Error: $error</h3>";
   }
+} else {
+  $userRepository->createUser($username, $password, $email);
+  echo "<h3 class='success'>Your account has been created!</h3>";
 }
-
-require_once './php_components/footer.php';
 ?>
+</div>
+
+<?php require_once './php_components/footer.php'; ?>
